55bb50a3eaf1f400a52626a676e0265f7fb94c1d
scergun <sametcaglarergun@gmail.com>
2023-10-07 23:07:52 +0300

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://scergun.github.io/many-things-app-react",
+  base: "/many-things-app-react/",
 });
